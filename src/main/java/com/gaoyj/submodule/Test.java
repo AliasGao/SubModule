128a942ce20693ebe71c1b75ecc2f597cf9cbd05
gaoyunjie <gaoyunjie@deppon.com>
2021-08-10 11:02:06 +0800

    目录提交

---
+++ src/main/java/com/gaoyj/submodule/Test.java
@@ -0,0 +1,18 @@
+package com.gaoyj.submodule;
+
+/**
+ * 测试
+ * @author 517987
+ * @date 2021/8/10
+ */
+public enum Test {
+    INSTANCE;
+    public static Test getInstance(){
+        return INSTANCE;
+    }
+
+    public void test(){
+
+    }
+
+}
